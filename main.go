584e4574d780ca95019478790e3161a3a6b7bef1
Darshan Mistry <159809544+1111darshan@users.noreply.github.com>
2024-08-24 12:21:53 +0000

    add more codes

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
+package main
+import "fmt"
+func main(){
+	fmt.Prinln("Hello world")
+}
+=======
 package main
 import "fmt"
 func main(){
 	fmt.Println("Hello world")
 }
+>>>>>>> a9c5a484de0a5c1a83d2c86e7a8868365ba64cad
